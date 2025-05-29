64adb85205a56c1dbe004aad4444293b67ee19e6
samylledutra <133446460+samylledutra@users.noreply.github.com>
2025-05-28 22:06:06 -0300

    first commit

---
+++ Perceptron_Digitos/src/perceptron/Perceptron_Digitos.java
@@ -0,0 +1,18 @@
+package perceptron;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Samylle
+ */
+public class Perceptron_Digitos {
+
+    public static void main(String[] args) {
+         InterfacePerceptron prc = new InterfacePerceptron();
+    }
+    
+}
+
